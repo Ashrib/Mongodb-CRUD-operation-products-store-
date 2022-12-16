eaaabe69b02acc89729c90b837022a566b0556fc
Syed Kariz Ali <karizali310@gmail.com>
2022-12-16 18:35:00 +0500

    updates

--- products/src/App.jsx
+++ products/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 
   const getAllProducts = async () => {
     try {
-      const response = await axios.get(`http://localhost:3000/products`)
+      const response = await axios.get(`${baseUrl}/products`)
       console.log("response: ", response.data);
 
       setProducts(response.data.data.reverse())
